80e046d83c232bc652dc5d7c670a1c50750066b3
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-05-15 04:28:39 +0300

    gives a brief intro about Django lib of Python

---
+++ Intro.py
@@ -0,0 +1,24 @@
+# Django Tutorial
+# Django is a back-end server side web framework.
+# Django is free, open source and written in Python.
+# Django makes it easier to build web pages using Python.
+# Learning by Doing
+# In this tutorial you get a step by step guide on how to install and create a Django project. 
+# You will learn how to create a project where you can add, read, update or delete data.
+# You will learn how to make HTML Templates and use Django Template Tags to insert data within a HTML document.
+# You will learn how to work with QuerySets to extract, filter, and sort data from the database.
+# You will also learn how to set up a PostgreSQL database and how to deploy your Django project to the world.
+# Django Display Admin Syntax QuerySets Static Files PostgreSQL Deploy
+
+# Django Exercises
+# Exercise:
+# Insert the missing parts to write a Django variable in a template:
+
+#<h1>Hello firstname ,how are you?</h1>
+
+# Example 2
+"""<ul>
+    {% for x in mymembers %}
+    <li>{{ x.firstname }}</li>
+  {% endfor %}
+</ul>"""
